373ee36e889c1d9238ba308a1476ff572980c6ff
zhengzhijian <1261383096@qq.com>
2019-04-28 21:04:51 +0800

    init gdusys-manage

---
+++ gdusys-admin/src/main/java/com/gdusys/common/aspect/DataFilterAspect.java
@@ -0,0 +1,123 @@
+/**
+ * Copyright (c) 2016-2019 人人开源 All rights reserved.
+ *
+ * https://www.renren.io
+ *
+ * 版权所有，侵权必究！
+ */
+
+package com.gdusys.common.aspect;
+
+
+import com.gdusys.common.annotation.DataFilter;
+import com.gdusys.common.exception.RRException;
+import com.gdusys.common.utils.Constant;
+import com.gdusys.modules.sys.entity.SysUserEntity;
+import com.gdusys.modules.sys.service.SysDeptService;
+import com.gdusys.modules.sys.service.SysRoleDeptService;
+import com.gdusys.modules.sys.service.SysUserRoleService;
+import com.gdusys.modules.sys.shiro.ShiroUtils;
+import org.apache.commons.lang.StringUtils;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 数据过滤，切面处理类
+ *
+ * @author Mark sunlightcs@gmail.com
+ */
+@Aspect
+@Component
+public class DataFilterAspect {
+    @Autowired
+    private SysDeptService sysDeptService;
+    @Autowired
+    private SysUserRoleService sysUserRoleService;
+    @Autowired
+    private SysRoleDeptService sysRoleDeptService;
+
+    @Pointcut("@annotation(com.gdusys.common.annotation.DataFilter)")
+    public void dataFilterCut() {
+
+    }
+
+    @Before("dataFilterCut()")
+    public void dataFilter(JoinPoint point) throws Throwable {
+        Object params = point.getArgs()[0];
+        if(params != null && params instanceof Map){
+            SysUserEntity user = ShiroUtils.getUserEntity();
+
+            //如果不是超级管理员，则进行数据过滤
+            if(user.getUserId() != Constant.SUPER_ADMIN){
+                Map map = (Map)params;
+                map.put(Constant.SQL_FILTER, getSQLFilter(user, point));
+            }
+
+            return ;
+        }
+
+        throw new RRException("数据权限接口，只能是Map类型参数，且不能为NULL");
+    }
+
+    /**
+     * 获取数据过滤的SQL
+     */
+    private String getSQLFilter(SysUserEntity user, JoinPoint point){
+        MethodSignature signature = (MethodSignature) point.getSignature();
+        DataFilter dataFilter = signature.getMethod().getAnnotation(DataFilter.class);
+        //获取表的别名
+        String tableAlias = dataFilter.tableAlias();
+        if(StringUtils.isNotBlank(tableAlias)){
+            tableAlias +=  ".";
+        }
+
+        //部门ID列表
+        Set<Long> deptIdList = new HashSet<>();
+
+        //用户角色对应的部门ID列表
+        List<Long> roleIdList = sysUserRoleService.queryRoleIdList(user.getUserId());
+        if(roleIdList.size() > 0){
+            List<Long> userDeptIdList = sysRoleDeptService.queryDeptIdList(roleIdList.toArray(new Long[roleIdList.size()]));
+            deptIdList.addAll(userDeptIdList);
+        }
+
+        //用户子部门ID列表
+        if(dataFilter.subDept()){
+            List<Long> subDeptIdList = sysDeptService.getSubDeptIdList(user.getDeptId());
+            deptIdList.addAll(subDeptIdList);
+        }
+
+        StringBuilder sqlFilter = new StringBuilder();
+        sqlFilter.append(" (");
+
+        if(deptIdList.size() > 0){
+            sqlFilter.append(tableAlias).append(dataFilter.deptId()).append(" in(").append(StringUtils.join(deptIdList, ",")).append(")");
+        }
+
+        //没有本部门数据权限，也能查询本人数据
+        if(dataFilter.user()){
+            if(deptIdList.size() > 0){
+                sqlFilter.append(" or ");
+            }
+            sqlFilter.append(tableAlias).append(dataFilter.userId()).append("=").append(user.getUserId());
+        }
+
+        sqlFilter.append(")");
+
+        if(sqlFilter.toString().trim().equals("()")){
+            return null;
+        }
+
+        return sqlFilter.toString();
+    }
+}
